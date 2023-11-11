8b7dd7d1c4ad335fb0af9caa9ff91ed017ca2257
nicklasmc <117492402+nicklasmc@users.noreply.github.com>
2023-11-10 16:44:44 -0800

    Update nchiang.h

--- nchiang.h
+++ nchiang.h
@@ -1,12 +1,12 @@
 #ifndef _NICKLAS_C_
 #define _NICKLAS_C_
 
-enum screen_list {
+/*enum screen_list {
     startup,
     play_game,
     credits,
     endgame
-};
+};*/
 
 extern void display_name(int x, int y);
 extern int get_last_mouse_movement(const bool yes);
@@ -15,9 +15,9 @@ extern void jumping();
 extern void updateJump();
 
 //Screens
-//extern void startup();
-//extern void credits();
-//extern void endgame();
+extern void startup();
+extern void credits();
+extern void endgame();
 extern void check_hp(int hp);
 
 #endif
